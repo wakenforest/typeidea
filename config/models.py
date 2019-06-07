2f3044cfa62d1a6730ff0649449b454de64a3ad2
wakenforest <wangfei_thuee@126.com>
2019-06-07 19:48:41 +0800

    增加最热文章

--- config/models.py
+++ config/models.py
@@ -55,7 +55,7 @@ class SideBar(models.Model):
         (4, '最近评论'),
     )
     title = models.CharField(max_length=50, verbose_name="标题")
-    display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE,
+    display_type = models.PositiveIntegerField(default=3, choices=SIDE_TYPE,
                                                verbose_name="展示类型")
     content = models.CharField(max_length=500, blank=True, verbose_name="内容",
                                help_text = "如果设置的不是HTML类型，可为空")
