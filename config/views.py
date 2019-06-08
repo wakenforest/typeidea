d63d61fbc1e70ac7e0d4f401a5e0f838ab162f0e
wakenforest <wangfei_thuee@126.com>
2019-06-08 10:20:23 +0800

    把友情链接改回正常模式（view.py中的template_name的link1.html对应ajax test文件）

--- config/views.py
+++ config/views.py
@@ -7,7 +7,7 @@ from .models import Link
 
 class LinkListView(CommonViewMixin, ListView):
     queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
-    template_name = 'config/links1.html'
+    template_name = 'config/links.html'
     context_object_name = 'link_list'
 
 import json
